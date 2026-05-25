e3365eb9bd47c020f338c20eb214f3d427ef22df
Gian Carlo Pacayra <gianpacayra@gmail.com>
2026-05-26 01:11:22 +0800

    v3.0.3 — bump version + sync USB serial number

--- src/usb_hid.rs
+++ src/usb_hid.rs
@@ -34,7 +34,7 @@ impl<'a> UsbHid<'a> {
         let device = UsbDeviceBuilder::new(bus, UsbVidPid(USB_VID, USB_PID))
             .manufacturer("GosuDRM")
             .product("Air96 V2 Keyboard")
-            .serial_number("v3.0.0")
+            .serial_number("v3.0.3")
             .device_class(0x00)
             .device_sub_class(0x00)
             .device_protocol(0x00)
